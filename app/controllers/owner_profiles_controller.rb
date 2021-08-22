1d0a67f591ea6acf51b88d9608dfdc7e1b95a386
Oleksii Lukash <lukash2213@gmail.com>
2021-08-22 12:19:33 +0000

    create view sitter profile (How Owner see it)

---
+++ app/controllers/owner_profiles_controller.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class OwnerProfilesController < ApplicationController
+  def show; end
+end
---
+++ app/controllers/sitter_profiles_controller.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class SitterProfilesController < ApplicationController
+  def show; end
+end
